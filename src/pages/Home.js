932b17f066dd5f321d01a1383e8c5dc73507bf46
Kristina <you@example.com>
2023-03-04 16:35:11 +0100

    Updates

--- src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home ({images}){
-    const homeImg = images.filter((img) => img.type === 'home')[0].location
-    return (
-        <div className="container"> 
-            <img src ={homeImg} alt="slika" className='img-fluid mx-auto d-block'/>
-        </div>
-    )
-}
